ab41f14e4d975dd0ef44bced1bf39b4a8e657c07
JainaraCamara <jainaranc@gmail.com>
2021-05-30 10:47:05 -0300

    Removing comments

--- Python - BCC740/String.py
+++ Python - BCC740/String.py
@@ -1,11 +1,5 @@
-#1)Considere a string A = "Um elefante incomoda muita gente". Que fatia corresponde a "elefante
-#incomoda"
-
 A = "Um elefante incomoda muita gente"
 print(A[3:20])
-
-#Escreva um programa que solicite uma frase ao usuário e escreva a frase toda em maiúscula e 
-#sem espaços em branco
 
 frase = input("Digite uma frase: ")
 print(frase.lower().replace(" ", ""))
