24e3cff257fa7f725001897318a71c9d451de988
JainaraCamara <jainaranc@gmail.com>
2021-05-30 17:49:41 -0300

    Create Dicionários.py

---
+++ Python - BCC740/Dicionários.py
@@ -0,0 +1,17 @@
+Lanchonete = {"Salgado": 4.50, 
+              "Lanche": 6.50, 
+              "Suco": 3.00, 
+              "Refrigerante": 3.50, 
+              "Doce": 1.00}
+print(Lanchonete)
+
+print("\n\n")
+
+Notas = {"Maria": 9.0,
+         "Pedro": 7.0,
+         "Lucas": 5.0,
+         "Fábio": 2.0,
+         "Carla": 6.0}
+print(Notas)
+media = (Notas["Maria"] + Notas["Pedro"] + Notas["Lucas"] + Notas["Fábio"] + Notas["Carla"])/5
+print("A média das notas dos alunos é ", media)
