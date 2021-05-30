8da3cdffb3fa66cf63921ee8cfd6c75300e1e15d
JainaraCamara <jainaranc@gmail.com>
2021-05-30 20:43:45 -0300

    Update Funções

---
+++ Python - BCC740/Funções.py
@@ -0,0 +1,17 @@
+#Exercício 1
+
+def desenho(N):
+    for i in range(N):
+        print (end = '_')
+
+n = int(input("Digite um número: "))
+desenho(n)
+print("\n")
+
+#Exercício 2 
+
+
+
+
+#Exercício 3
+
