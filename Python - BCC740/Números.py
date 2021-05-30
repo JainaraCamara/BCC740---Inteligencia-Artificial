4062d4f01754ad021cb12ece693f2f93f3cc879b
JainaraCamara <jainaranc@gmail.com>
2021-05-30 10:39:26 -0300

    Create Números.py

---
+++ Python - BCC740/Números.py
@@ -0,0 +1,11 @@
+#numeros
+
+x = int(input("X: "))
+y = int(input("Y: "))
+
+if x != y:
+    z = ((x**2)+(y**2))/((x-y)**2)
+    print("O valor de Z é:", int(z))
+
+else:
+    print("IMPOSSÍVEL CALCULAR O VALOR DE Z: NÃO EXISTE DIVISÃO POR 0!")
