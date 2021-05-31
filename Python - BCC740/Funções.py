8535a19c2c35975a43fac14e0b9639ebc840d8e6
JainaraCamara <jainaranc@gmail.com>
2021-05-31 14:22:27 -0300

    Update Funções.py

--- Python - BCC740/Funções.py
+++ Python - BCC740/Funções.py
@@ -10,8 +10,19 @@ print("\n")
 
 #Exercício 2 
 
+def list(L):
+    for i in range (len(L)):
+        print(i + 1," - ",L[i])
 
+    
+list(["abacate","melão","abacaxi","melão", 9, 6.3])
+print("\n")
 
 
 #Exercício 3
 
+def media(Lnum):
+    media = sum(Lnum)/len(Lnum)
+    return media
+
+print("Média dos valores: ", media([10,10,5,5,5]))
